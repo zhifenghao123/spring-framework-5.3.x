8622b287cb36a69e02a9d4562722d5416aabf347
zhifenghao123 <zhifenghao123@outlook.com>
2023-02-23 13:01:52 +0800

    半配置半注解方式结合annotation-config 配置Spring

---
+++ spring-hao-test-beanfactory/src/main/java/com/hao/xml4/component/Product.java
@@ -0,0 +1,50 @@
+package com.hao.xml4.component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * Product class
+ *
+ * @author haozhifeng
+ * @date 2023/02/23
+ */
+public class Product {
+    private String productId;
+    private String productName;
+    private double price;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("in Product : @PostConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("in Product : @PreDestroy");
+    }
+}
