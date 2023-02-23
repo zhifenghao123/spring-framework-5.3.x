e7c14d7c223c3ceb89344345a9d335fa8a7f4cd9
zhifenghao123 <zhifenghao123@outlook.com>
2023-02-23 11:48:58 +0800

    新增spring-hao-test-beanfactory模块

---
+++ spring-hao-test-beanfactory/src/main/java/com/hao/xml1/ClassPathXmlApplicationContextMain1.java
@@ -0,0 +1,8 @@
+package com.hao.xml1;/**
+ * ClassPathXmlApplicationContextMain1 class
+ *
+ * @author haozhifeng
+ * @date 2023/02/23
+ */
+public class ClassPathXmlApplicationContextMain1 {
+}
---
+++ spring-hao-test-beanfactory/src/main/java/com/hao/xml1/component/Product.java
@@ -0,0 +1,8 @@
+package com.hao.xml1.component;/**
+ * Product class
+ *
+ * @author haozhifeng
+ * @date 2023/02/23
+ */
+public class Product {
+}
