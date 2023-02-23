b42278095171988dda092583e0f446bb4def62c1
zhifenghao123 <zhifenghao123@outlook.com>
2023-02-23 14:14:22 +0800

    打印容器中的所有BeanDefinition

--- spring-hao-test-beanfactory/src/main/java/com/hao/xml1/ClassPathXmlApplicationContextMain1.java
+++ spring-hao-test-beanfactory/src/main/java/com/hao/xml1/ClassPathXmlApplicationContextMain1.java
@@ -1,5 +1,6 @@
 package com.hao.xml1;
 
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.hao.xml1.component.Order;
@@ -18,7 +19,17 @@ public class ClassPathXmlApplicationContextMain1 {
                 new ClassPathXmlApplicationContext("applicationContext-1.xml");
         Product product = xmlApplicationContext.getBean(Product.class);
         Order order = xmlApplicationContext.getBean(Order.class);
+
+        System.out.println("--------------------");
+        String[] beanDefinitionNames = xmlApplicationContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition =
+                    xmlApplicationContext.getBeanFactory().getBeanDefinition(beanDefinitionName);
+            System.out.println(beanDefinition.getBeanClassName());
+        }
+        System.out.println("--------------------");
         System.out.println(product);
         System.out.println(order);
+        System.out.println("--------------------");
     }
 }
