2b071340c23992537f09b42293b2e7999f78efd1
zhifenghao123 <zhifenghao123@outlook.com>
2023-02-27 14:07:13 +0800

    @Configuration和@Bean注解联合使用

---
+++ spring-hao-test-beanfactory/src/main/java/com/hao/javaConfig1/component/Product.java
@@ -0,0 +1,41 @@
+package com.hao.javaConfig1.component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * Product class
+ *
+ * @author haozhifeng
+ * @date 2023/02/23
+ */
+public class Product {
+    private String productId;
+    private String productName;
+    private double price;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
---
+++ spring-hao-test-beanfactory/src/main/java/com/hao/javaConfig2/AppConfig.java
@@ -0,0 +1,56 @@
+package com.hao.javaConfig1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.hao.javaConfig1.component.Comment;
+import com.hao.javaConfig1.component.Order;
+import com.hao.javaConfig1.component.Product;
+
+/**
+ * AppConfig class
+ * Spring3.0开始，@Configuration用于定义配置类，定义的配置类可以替换xml文件，一般和@Bean注解联合使用。
+ * @Configuration注解主要标注在某个类上，相当于xml配置文件中的<beans>
+ * @Bean注解主要标注在某个方法上，相当于xml配置文件中的<bean>
+ *
+ * @Configuration注解的配置类有如下要求：
+ * （1）@Configuration不可以是final类型；
+ * （2）@Configuration不可以是匿名类；
+ * （3）嵌套的configuration必须是静态类。
+ *
+ * SpringBoot社区推荐使用基于JavaConfig的配置形式，所以，启动类标注了@Configuration之后，本身其实也是一个IoC容器的配置类。
+ *
+ * @author haozhifeng
+ * @date 2023/02/23
+ */
+@Configuration
+//@EnableAspectJAutoProxy//(proxyTargetClass = true)
+public class AppConfig {
+    @Bean
+    Product product() {
+        Product product = new Product();
+        product.setProductId("javaConfig1_procuct_111");
+        product.setProductName("orange");
+        product.setPrice(3.5);
+        return product;
+    }
+
+    @Bean
+    Order order() {
+        Order order = new Order();
+        order.setUserAccountId("javaConfig1_userAccount_111");
+        order.setOrderId("javaConfig1_orderId_111");
+        order.setProduct(product());
+        return order;
+    }
+
+    @Bean
+    Comment comment() {
+        Comment comment = new Comment();
+        comment.setCommentId("javaConfig1_comment_111");
+        comment.setCommentText("很棒");
+        comment.setProduct(product());
+        return comment;
+    }
+}
