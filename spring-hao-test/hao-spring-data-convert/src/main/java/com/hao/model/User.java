c2e5a4a4012318d5b0ba3a245eeb3a4e7bffe0ab
zhifenghao123 <zhifenghao123@outlook.com>
2023-05-30 16:13:01 +0800

    新增User

--- spring-hao-test/hao-spring-data-convert/src/main/java/com/hao/model/User.java
+++ spring-hao-test/hao-spring-data-convert/src/main/java/com/hao/model/User.java
@@ -1,8 +1,36 @@
-package com.hao.model;/**
+package com.hao.model;
+
+import java.io.Serializable;
+
+/**
  * User class
  *
  * @author haozhifeng
  * @date 2023/05/30
  */
-public class User {
+public class User implements Serializable {
+    /**
+     * 姓名
+     */
+    private String name;
+    /**
+     * 年龄
+     */
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
